7452295ef5d78440184fa290d62ead6410d5cd44
fistkim101 <fistkim101@gmail.com>
2023-10-10 11:08:46 +0900

    feat: 인프라 모듈 테스트 코드 작성

--- fistkim-bank-infrastructure/src/test/java/com/fistkim/saving/adapter/out/persistence/account/AccountPersistenceAdapterTest.java
+++ fistkim-bank-infrastructure/src/test/java/com/fistkim/saving/adapter/out/persistence/account/AccountPersistenceAdapterTest.java
@@ -46,17 +46,35 @@ class AccountPersistenceAdapterTest {
     @DisplayName("계좌 해지 검증")
     @Test
     void closeAccount() {
+        final Long OWNER_ID = 1L;
+        final AccountType ACCOUNT_TYPE = AccountType.NORMAL;
+        final Money INITIAL_MONEY = Money.of(1000);
+        final Account account = Account.open(OWNER_ID, ACCOUNT_TYPE, INITIAL_MONEY);
+        final Account openedAccount = this.accountPersistenceAdapter.openAccount(account);
+        final Optional<AccountJpaEntity> openedAccountJpaEntity = this.accountJpaRepository.findById(openedAccount.getId());
+        Assertions.assertThat(openedAccountJpaEntity).isNotEmpty();
 
-
-
+        final Long accountId = openedAccountJpaEntity.get().getId();
+        this.accountPersistenceAdapter.closeAccount(accountId);
+        final Optional<AccountJpaEntity> closedAccountJpaEntity = this.accountJpaRepository.findById(accountId);
+        Assertions.assertThat(closedAccountJpaEntity).isEmpty();
     }
 
-//
-//    @Test
-//    void updateAccountBalance() {
-//    }
-//
-//    @Test
-//    void loadAccount() {
-//    }
+    @DisplayName("계좌 잔액 변경 검증")
+    @Test
+    void updateAccountBalance() {
+        final Long OWNER_ID = 1L;
+        final AccountType ACCOUNT_TYPE = AccountType.NORMAL;
+        final Money INITIAL_MONEY = Money.of(1000);
+        final Account account = Account.open(OWNER_ID, ACCOUNT_TYPE, INITIAL_MONEY);
+        final Account openedAccount = this.accountPersistenceAdapter.openAccount(account);
+
+        openedAccount.deposit(Money.of(1000));
+        this.accountPersistenceAdapter.updateAccountBalance(openedAccount.getId(), openedAccount.getBalance());
+
+        final Optional<AccountJpaEntity> openedAccountJpaEntity = this.accountJpaRepository.findById(openedAccount.getId());
+        Assertions.assertThat(openedAccountJpaEntity).isNotEmpty();
+        Assertions.assertThat(openedAccountJpaEntity.get().getBalance()).isEqualTo(2000);
+    }
+
 }
